474e2a9d999ea0ecfcb251831a8b062b0ac53cda
Runshi Gu <justingu14053@gmail.com>
2022-12-12 22:12:21 -0500

    drag added

--- Assets/BoxPackage/BallBehavior.cs
+++ Assets/BoxPackage/BallBehavior.cs
@@ -7,12 +7,17 @@ public class BallBehavior : MonoBehaviour
 
     public float MinSpeed = 2;
     public float MaxSpeed = 10;
+    public float SlowThresholdRange = 3;
+    public float constantDrag = 0.2f;
 
     private Rigidbody rb;
+    private GameObject PlayerCamera;
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
+        PlayerCamera = FindObjectOfType<Camera>().gameObject;
+        constantDrag = rb.drag;
     }
 
     // Update is called once per frame
@@ -24,6 +29,14 @@ public class BallBehavior : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, MaxSpeed);
+
+        // slow the ball down when it get close to the player
+        float distance = (transform.position - PlayerCamera.transform.position).magnitude;
+        if (distance < SlowThresholdRange)
+        {
+            rb.drag = constantDrag * (SlowThresholdRange / distance);
+        }
+
         //todo min
         if(rb.velocity.magnitude < MinSpeed)
         {
