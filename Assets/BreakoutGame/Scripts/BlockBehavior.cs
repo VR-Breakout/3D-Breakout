3c8b08351f3b0775cd114a5b322f2fe8b37c8633
Runshi Gu <justingu14053@gmail.com>
2022-12-13 22:06:34 -0500

    Particle system and collision sounds

---
+++ Assets/BreakoutGame/Scripts/BlockBehavior.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehavior : MonoBehaviour
+{
+    private Global Global;
+
+    public ParticleSystem Particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Global = GameObject.Find("Global").GetComponent<Global>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(Particle != null)
+        {
+            Instantiate(Particle, transform.position, Quaternion.identity);
+        }
+        Global.score += 10;
+        Destroy(gameObject, 0.1f);
+    }
+}
---
+++ Assets/BreakoutGame/Scripts/PaddleBehavior.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public enum Handedness { left, right }
+
+public class PaddleBehavior : MonoBehaviour
+{
+    Vector3 previousPos;
+    private float velocity;
+
+    public GameObject Controller;
+    public Handedness handedness;
+    private HapticCapabilities caps;
+
+    public GameObject global;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Controller == null)
+        {
+            Debug.Log("Controller is null, needs to be assigned");
+        }
+        else
+        {
+            caps = new HapticCapabilities();
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Transform newT = Controller.transform;
+        this.GetComponent<Rigidbody>().MovePosition(newT.position);
+        this.GetComponent<Rigidbody>().MoveRotation(newT.rotation);
+
+        Vector3 displace = (this.transform.position - previousPos);
+        velocity = displace.magnitude / Time.deltaTime;
+
+        previousPos = this.transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ball"))
+        {
+            if (global != null)
+            {
+                global.GetComponent<Global>().numHits += 1;
+            }
+
+            Vector3 normal = -collision.GetContact(0).normal;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(normal * velocity); //assume contact is instantaenous? f * t
+
+            XRNode node;
+            if (handedness == Handedness.left)
+            {
+                node = XRNode.LeftHand;
+            }
+            else
+            {
+                node = XRNode.RightHand;
+            }
+            if (InputDevices.GetDeviceAtXRNode(node).TryGetHapticCapabilities(out caps))
+            {
+                if (caps.supportsImpulse)
+                {
+                    InputDevices.GetDeviceAtXRNode(node).SendHapticImpulse(0, 0.3f, 0.3f); //channel, amp, seconds
+                }
+            }
+        }
+    }
+
+}
